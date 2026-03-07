c811a92c334d697cc5a2dbbe9c94b5369b98f13b
Mona Dmitriy <skorobei91@mail.ru>
2026-03-07 22:05:04 +0300

    DML day1

---
+++ DML/SQLQuery2-select.sql
@@ -0,0 +1,10 @@
+--SQLQuery2-select.sql
+USE PV_522_Import;
+--ALTER TABLE		Groups
+--ALTER COLUMN	start_time TIME(0) NOT NULL;
+
+SELECT	group_name	AS N'Группа',
+		direction_name	AS	N'Направление обучения'
+FROM	Groups, Directions
+WHERE	direction = direction_id
+;
