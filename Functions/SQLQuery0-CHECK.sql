211fc40bbd30a17b6cc2c5b5b4f4f37f5d629ff7
Mona Dmitriy <skorobei91@mail.ru>
2026-03-21 21:14:26 +0300

    GetNextLearningDay DONE

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -1,5 +1,7 @@
 --SQLQuery0-CHECK.sql
 USE PV_522_Import;
 
-PRINT dbo.GetLastLearningDate(N'PV_522');
+--PRINT dbo.GetLastLearningDate(N'PV_522');
 --EXEC sp_SelectSchedule;
+
+PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
